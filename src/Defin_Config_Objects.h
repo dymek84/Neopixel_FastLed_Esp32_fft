215333ba9b963a08d0f770c7000dd7e93a2a6007
dymek84 <dymek1984@gmail.com>
2023-02-10 20:06:06 +0100

    ccc

---
+++ src/Defin_Config_Objects.h
@@ -0,0 +1,6 @@
+
+#define SAMPLES 512 // Must be a power of 2 (64)
+#define MULTIPLY_BY 1
+#define xres 32 // Total number of  columns in the display
+#define yres 8  // Total number of  rows in the display
+#define audio 35
